e611e3a95a5f12fdc724fff25235836236159a23
pool <epsilonsunny@gmai.com>
2019-01-02 18:42:04 +0800

    on

---
+++ study_java/projects/scala_test01/src/main/scala/MyWordCout.scala
@@ -0,0 +1,12 @@
+/**
+  * @Classname MyWordCout
+  * @Description TODO
+  * @Date 2019/1/2 3:20 PM
+  * @Created by pool
+  */
+object MyWordCout {
+  def main(args: Array[String]): Unit = {
+    print("hello spark")
+  }
+
+}
