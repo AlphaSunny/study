dd8a5d9ccf189d6b8abc6cfe8fd992c00ede4a8f
pool <epsilonsunny@gmai.com>
2019-01-03 17:13:50 +0800

    on

---
+++ study_java/projects/spark_test01/src/main/scala/com/hivebanks/day01/WorldCount.scala
@@ -0,0 +1,14 @@
+package com.hivebanks.day01
+
+/**
+  * @Classname WorldCount
+  * @Description TODO
+  * @Date 2019/1/3 11:07 AM
+  * @Created by pool
+  */
+object WorldCount {
+  def main(args: Array[String])  {
+    print("hello spark")
+  }
+
+}
---
+++ study_java/projects/spark_test05/src/main/scala/com/hivebanks/test/spark_hello.scala
@@ -0,0 +1,14 @@
+package com.hivebanks.test
+
+/**
+  * @Classname spark_hello
+  * @Description TODO
+  * @Date 2019/1/3 5:08 PM
+  * @Created by pool
+  */
+object spark_hello {
+  def main(args: Array[String]): Unit = {
+    print("yang")
+  }
+
+}
